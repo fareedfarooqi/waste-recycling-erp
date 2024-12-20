48fd9fbbd4c8fba6a04d3406fa7726e574e84fff
Fareed Farooqi <z5425725@ad.unsw.edu.au>
2024-12-20 19:21:48 +1100

    Created Navbar, Sidebar components and created client-list route

---
+++ my-app/app/client-list/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+function ClientListPage(): JSX.Element {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Navbar;
