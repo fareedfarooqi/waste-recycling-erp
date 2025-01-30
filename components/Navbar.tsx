7d880d02ef5bbb187cf1b66e80e08d79f51d1df7
Sriram <srisur04@gmail.com>
2025-01-30 20:32:21 +1100

    removed navbar from mod 5 files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,22 @@
-'use client';
-import React from 'react';
-import { GoSearch } from 'react-icons/go';
-import { useSidebar } from '@/context/SidebarContext';
+// 'use client';
+// import React from 'react';
+// import { GoSearch } from 'react-icons/go';
+// import { useSidebar } from '@/context/SidebarContext';
 
-const Navbar = (): JSX.Element => {
-    const { isSidebarOpen } = useSidebar();
+// const Navbar = (): JSX.Element => {
+//     const { isSidebarOpen } = useSidebar();
 
-    return (
-        <div
-            className={`flex ${isSidebarOpen ? 'ml-16' : 'ml-16'} transition-all duration-100`}
-        >
-            <div className="flex-1">
-                <nav className="bg-green-50 p-4 flex items-center">
-                    <span className="mr-5 text-white hover:scale-110 cursor-pointer transition-all"></span>
-                </nav>
-            </div>
-        </div>
-    );
-};
+//     return (
+//         <div
+//             className={`flex ${isSidebarOpen ? 'ml-16' : 'ml-16'} transition-all duration-100`}
+//         >
+//             <div className="flex-1">
+//                 <nav className="bg-green-50 p-4 flex items-center">
+//                     <span className="mr-5 text-white hover:scale-110 cursor-pointer transition-all"></span>
+//                 </nav>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Navbar;
+// export default Navbar;
